b05e78c16613f274aa86db952e6a080f8514a122
bridgesmith17 <bridgesmith17@gmail.com>
2016-05-14 13:36:11 -0700

    correction

--- demo.js
+++ demo.js
@@ -1,26 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf460
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red242\green242\blue242;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl360\partightenfactor0
-
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 \{\
-  "name": "hello-world",\
-  "version": "1.0.0",\
-  "description": "Hello world with express and node.js",\
-  "main": "app.js",\
-  "scripts": \{\
-    "test": "echo \\"Error: no test specified\\" && exit 1"\
-  \},\
-  "author": "OSU",\
-  "license": "ISC",\
-  "dependencies": \{\
-    "body-parser": "^1.13.3",\
-    "express": "^4.13.2",\
-    "express-session": "^1.11.3",\
-    "mysql": "^2.8.0"\
-  \}\
-\}\
+{
+  "name": "hello-world",
+  "version": "1.0.0",
+  "description": "Hello world with express and node.js",
+  "main": "app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "OSU",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.13.3",
+    "express": "^4.13.2",
+    "express-session": "^1.11.3",
+    "mysql": "^2.8.0"
+  }
 }
